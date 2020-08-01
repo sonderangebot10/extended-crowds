ac15e9e3f159d6a7a8abfa8b5f2269d5fad4b6f1
Viktoriya Kutsarova <viktoriya.kutsarova@gmail.com>
2020-08-01 14:32:46 +0300

    Remove redirection of error output.

--- CrowdS Server/PHP/www/html/login.php
+++ CrowdS Server/PHP/www/html/login.php
@@ -66,12 +66,13 @@ else{
         if ($server_os === "Windows") {
             $initheartbeatcommand = "schtasks.exe /Create /st ".$future." /tn ".$taskid." /sc ONCE /tr \"php".ROOT_PATH."html\heartbeat_check.php ".$id."\" 2>&1";
         } else if ($server_os === "Linux") {
-            $initheartbeatcommand = 'echo "php '.ROOT_PATH.'html/heartbeat.php '.$id.'" | at '.$future.' 2>&1';
+            $initheartbeatcommand = 'echo "php '.ROOT_PATH.'html/heartbeat.php '.$id.'" | at '.$future;
         } else {
             error_log("Unsupported OS.");
         }
         error_log("Init heartbeat: " . $initheartbeatcommand);
         $atjob = exec($initheartbeatcommand);
+        error_log("Init heartbeat2: " . $initheartbeatcommand);
         $at = explode(" ", $atjob);
         $atid = $at[1];
         error_log("atid: " . $atid);
