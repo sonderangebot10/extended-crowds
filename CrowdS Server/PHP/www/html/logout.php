c7cf676ea25eb97cefab1f9f389d1993a06c7d7e
viktoriya-kutsarova <viktoriya.kutsarova@gmail.com>
2020-11-13 15:32:52 +0200

    Fix deletion of heartbeat task on logout.

--- CrowdS Server/PHP/www/html/logout.php
+++ CrowdS Server/PHP/www/html/logout.php
@@ -1,9 +1,11 @@
 <?php
 include_once("config.php");
 include_once("database.php");
+include_once ("system_utils.php");
 
 $dbc = new DatabaseController();
 $dbc->connect();
+$server_os = getOSName();
 
 // id is sent from the mobile app
 $id = $dbc->escapeString($_POST["email"]);
@@ -11,8 +13,16 @@ $id = $dbc->escapeString($_POST["email"]);
 $heartbeat = $dbc->getUserFields($id, array("heartbeat"))["heartbeat"];
 
 // remove scheduled heartbeats
-list($atid,$timestamp) = explode(":", $heartbeat);
-exec("atrm ".$atid." 2>&1");
+list($old_id,$timestamp) = explode(":", $heartbeat);
+$deletetaskcommand = "";
+if ($server_os === "Windows") {
+    $deletetaskcommand = "schtasks.exe /Delete /tn ".$old_id." /f 2>&1";
+} else if ($server_os === "Linux") {
+    $deletetaskcommand = "atrm ".$old_id." 2>&1";
+} else {
+    error_log("Unsupported OS.");
+}
+exec($deletetaskcommand);
 
 // update logout time
 $dbc->updateUser($id, array("logout_time", "active"), array("NOW()", "0"));
